849aa6c0fbd11b2271282823e3f8a47a94f56fca
Guy Harel <guy@remarkety.com>
2020-08-14 22:47:58 +0300

    Add project files.

---
+++ pingpongclock.ino
@@ -0,0 +1,15 @@
+/*
+ Name:		pingpongclock.ino
+ Created:	8/14/2020 10:47:37 PM
+ Author:	guy
+*/
+
+// the setup function runs once when you press reset or power the board
+void setup() {
+
+}
+
+// the loop function runs over and over again until power down or reset
+void loop() {
+  
+}
